9e988b68bcd725e4431fdec62cefac61e3f9fd61
Paul Dickey <pauldjdickey@gmail.com>
2018-07-27 14:39:16 -0700

    Add Swipe Cell Controller

--- Alpaca/List Controllers/ListItemsTableViewController.swift
+++ Alpaca/List Controllers/ListItemsTableViewController.swift
@@ -1,14 +1,14 @@
 //
-//  ListTableViewController.swift
+//  ListItemsTableViewController.swift
 //  Alpaca
 //
-//  Created by Paul Dickey2 on 7/23/18.
+//  Created by Paul Dickey2 on 7/27/18.
 //  Copyright © 2018 Paul Dickey. All rights reserved.
 //
 
 import UIKit
 
-class ListTableViewController: UITableViewController {
+class ListItemsTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Alpaca/List Controllers/ListTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ListTableViewController.swift
+//  Alpaca
+//
+//  Created by Paul Dickey2 on 7/23/18.
+//  Copyright © 2018 Paul Dickey. All rights reserved.
+//
+
+import UIKit
+
+class ListTableViewController: SwipeTableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
