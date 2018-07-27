fe92b79365bd594131df21f36c0c77e2a536b750
Paul Dickey <pauldjdickey@gmail.com>
2018-07-27 16:16:42 -0700

    Did add items controller code. Search bar not working but w/ no errors.

--- Alpaca/List Controllers/ListItemsTableViewController.swift
+++ Alpaca/List Controllers/ListItemsTableViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import RealmSwift
 
-class ListItemsTableViewController: SwipeTableViewController, UISearchBarDelegate {
+class ListItemsTableViewController: SwipeTableViewController {
     
     var listItems: Results<Item>?
     let realm = try! Realm()
@@ -19,7 +19,7 @@ class ListItemsTableViewController: SwipeTableViewController, UISearchBarDelegat
     
     var selectedList : List? {
         didSet{
-           // loadItems()
+            loadItems()
         }
     }
 
@@ -30,4 +30,100 @@ class ListItemsTableViewController: SwipeTableViewController, UISearchBarDelegat
         title = selectedList?.name
     }
     
+    //MARK: - Tableview Datasource Methods (What to load into cells)
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listItems?.count ?? 1
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = super.tableView(tableView, cellForRowAt: indexPath)
+        
+        if let item = listItems?[indexPath.row] {
+            cell.textLabel?.text = item.title
+            cell.accessoryType = item.done ? .checkmark : .none
+        } else {
+            cell.textLabel?.text = "No items added"
+        }
+        return cell
+    }
+    
+    
+    //MARK: - TableView Delegate Methods (What happens when a cell is selected)
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if let item = listItems?[indexPath.row] {
+            do {
+                try realm.write {
+                    item.done = !item.done
+                }
+            } catch {
+                print("Error saving the done status \(error)")
+            }
+        }
+        tableView.reloadData()
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    //MARK: - Add items w/ Plus Button
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add new item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            if let currentList = self.selectedList {
+                do {
+                    try self.realm.write {
+                        let newItem = Item()
+                        newItem.title = textField.text!
+                        newItem.dateCreated = Date()
+                        currentList.items.append(newItem)
+                    }
+                } catch {
+                    print("Could not save new item \(error)")
+                }
+            }
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    //MARK: - Data Manipulaton (Load)
+    func loadItems() {
+        listItems = selectedList?.items.sorted(byKeyPath: "title", ascending: true)
+        tableView.reloadData()
+    }
+    
 }
+
+extension ListItemsTableViewController: UISearchBarDelegate {
+    //MARK: - Search bar methods
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print("Search Button Clicked")
+        listItems = listItems?.filter("title CONTAINS[cd] %@", searchBar.text!).sorted(byKeyPath: "dateCreated", ascending: true)
+        tableView.reloadData()
+        
+    }
+    
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+            
+        }
+    }
+}
+
