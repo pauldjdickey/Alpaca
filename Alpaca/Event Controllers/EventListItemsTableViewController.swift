5c5d2f88334b0a9fd902f9ecfd692aac161d502d
Paul Dickey <pauldjdickey@gmail.com>
2018-07-29 17:28:26 -0700

    Add event add task functionality that is organized by lists -> tasks. Segued with present so the done button dismisses the selection area.

---
+++ Alpaca/Event Controllers/EventListItemsTableViewController.swift
@@ -0,0 +1,103 @@
+//
+//  EventListItemsTableViewController.swift
+//  Alpaca
+//
+//  Created by Paul Dickey2 on 7/29/18.
+//  Copyright © 2018 Paul Dickey. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class EventListItemsTableViewController: SwipeTableViewController {
+    
+    var listItems: Results<Item>?
+    let realm = try! Realm()
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    var selectedList : List? {
+        didSet{
+            loadItems()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        loadItems()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        title = selectedList?.name
+    }
+    
+    //MARK: - Tableview Datasource Methods (What to load into cells)
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listItems?.count ?? 1
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = super.tableView(tableView, cellForRowAt: indexPath)
+        
+        if let item = listItems?[indexPath.row] {
+            cell.textLabel?.text = item.title
+            cell.accessoryType = item.added ? .checkmark : .none
+            if item.added == true {
+                cell.backgroundColor = UIColor.green
+            } else {
+                cell.backgroundColor = UIColor.clear
+            }
+        }
+        return cell
+    }
+    
+    
+    
+    //MARK: - TableView Delegate Methods (What happens when a cell is selected)
+        override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            if let item = listItems?[indexPath.row] {
+                do {
+                    try realm.write {
+                        if item.id == "" {
+                            item.id = "hello"
+                            item.added = true
+                        } else {
+                            item.id = ""
+                            item.added = false
+                        }
+                    }
+                } catch {
+                    print("Error saving the done status \(error)")
+                }
+            }
+            tableView.reloadData()
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
+    
+    //MARK: - Data Manipulaton (Load)
+    func loadItems() {
+        listItems = selectedList?.items.sorted(byKeyPath: "title", ascending: true)
+        tableView.reloadData()
+    }
+    
+    //MARK: - Delete Data From Swipe
+    
+    override func updateModel(at indexPath: IndexPath) {
+        
+        if let itemForDeletion = self.listItems?[indexPath.row] {
+            do {
+                try self.realm.write {
+                    self.realm.delete(itemForDeletion)
+                }
+            } catch {
+                print("Error deleting list, \(error)")
+            }
+        }
+    }
+    //MARK: - Done Button Pressed
+    @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Alpaca/Event Controllers/EventListTableViewController.swift
@@ -0,0 +1,91 @@
+//
+//  AllItemsTableViewController.swift
+//  Alpaca
+//
+//  Created by Paul Dickey2 on 7/29/18.
+//  Copyright © 2018 Paul Dickey. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class EventListTableViewController: SwipeTableViewController {
+
+    let realm = try! Realm()
+    var lists: Results<List>?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadLists()
+        print(Realm.Configuration.defaultConfiguration.fileURL!)
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        loadLists()
+    }
+    
+    //MARK: - TableView Datasource Methods (Code that tells each cell what to load)
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return lists?.count ?? 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = super.tableView(tableView, cellForRowAt: indexPath)
+        
+        if let list = lists?[indexPath.row] {
+            cell.textLabel?.text = list.name
+        }
+        return cell
+    }
+    
+    //MARK: - TableView Delegate Methods (Code that says what happens when we select a cell)
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "goToEventListItems", sender: self)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! EventListItemsTableViewController
+        
+        if let indexPath = tableView.indexPathForSelectedRow {
+            destinationVC.selectedList = lists?[indexPath.row]
+        }
+    }
+    
+    //MARK: - Data Manipulation Methods (Save and Load)
+    
+    func save(list: List) {
+        do {
+            try realm.write {
+                realm.add(list)
+            }
+        } catch {
+            print("Error saving list \(error)")
+        }
+        tableView.reloadData()
+    }
+    
+    func loadLists () {
+        lists = realm.objects(List.self)
+        tableView.reloadData()
+    }
+    
+    //MARK: - Delete Data From Swipe
+    
+    override func updateModel(at indexPath: IndexPath) {
+        
+        if let listForDeletion = self.lists?[indexPath.row] {
+            do {
+                try self.realm.write {
+                    self.realm.delete(listForDeletion)
+                }
+            } catch {
+                print("Error deleting list, \(error)")
+            }
+        }
+    }
+    
+    //MARK: - Done Button Pressed
+    @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
